90c91e80f53433cbf79e10cb5de4a78b3df245e2
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2026-02-16 10:27:01 -0500

    add mock data and API that will later be used for frontend

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/health')
 def health():
     return 'health'
+
+@app.get('/observation/<int:observation_id>')
+def fetch_observation(observation_id):
+    try:
+        file_name = 'observation_' + str(observation_id) + '.json'
+        with open(f'resources/{file_name}', 'r') as file:
+            data = json.load(file)
+        return data, 200
+    except FileNotFoundError as err:
+        print(f'file not found with observation id: {observation_id}, exception: {err}')
+        data = {
+        'message': 'Resource not found',
+        'status': 'BAD REQUEST'
+        }
+        return jsonify(data), 400
+    except json.JSONDecodeError as err:
+        print(f'error reading json file for observation id: {observation_id}, exception: {err}')
+        data = {
+        'message': 'JSON Decode Error',
+        'status': 'INTERNAL SERVER ERROR'
+        }
+        return jsonify(data), 500
