ca2d5a64d2c9d94814e0e95ec3ec9d4857ad9118
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2026-02-16 22:28:49 -0500

    Add useState

--- frontend/src/features/login/LoginPage.tsx
+++ frontend/src/features/login/LoginPage.tsx
@@ -1,24 +1,56 @@
 import { useNavigate, type NavigateFunction } from 'react-router-dom';
 import Button from '../../components/button/Button';
 import styles from './LoginPage.module.css';
+import { useState } from 'react';
 
 
 function LoginPage() {
     const navigate: NavigateFunction = useNavigate();
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [validLogin, setValidLogin] = useState(true);
+
+    function handleUsernameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
+
+    function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value);
+    }
+
+    function handleLoginClick() {
+        if (userName.length > 0 && password.length > 0) {
+            navigate('/home');
+        } else {
+            setValidLogin(false);
+        }
+    }
 
     return (
         <div className={styles.loginPage}>
             <div>
                 User Name
             </div>
-            <input className={styles.loginInput} placeholder='Enter username: '/>
+            <input  className={styles.loginInput} 
+                    placeholder='Enter username: '
+                    value={userName}
+                    onChange={handleUsernameChange}/>
             <div>
                 Password
             </div>
-            <input className={styles.loginInput} placeholder='Enter password: '/>
-            <Button onClick={() => navigate('/home')}>
+            <input  className={styles.loginInput} 
+                    placeholder='Enter password: '
+                    value={password}
+                    onChange={handlePasswordChange}/>
+            <Button onClick={handleLoginClick}>
                 Login
             </Button>
+            {
+                !validLogin && 
+                <div>
+                    Username and Password cannot be empty
+                </div>
+            }
         </div>
     )
 }
